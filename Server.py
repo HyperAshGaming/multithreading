61bf946d50b9914cc89db0c6b7d022a50dcda595
HyperAshGaming <HyperAshGaming@gmail.com>
2023-11-18 19:20:18 -0800

    making chat app with tkinter

--- Server.py
+++ Server.py
@@ -1,6 +1,11 @@
 import threading
 import socket
+from tkinter import *
 
+root = Tk()
+root.title("Chat Room")
+
+root.grid()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = ''
@@ -8,6 +13,9 @@ port = 12345
 s.bind((host,port))
 s.listen(5)
 conn, addr = s.accept()
+
+def PLACEHOLDER():
+    pass
 
 def SEND():
     while True:
@@ -22,5 +30,24 @@ def RECIEVE():
 t1 = threading.Thread(target = SEND)
 t2 = threading.Thread(target = RECIEVE)
 
-t1.start()
-t2.start()
+#Enter Username
+
+#Line
+
+#Enter Button
+
+Label1 = Label(root, text = "Enter a username", font = ("Arial",12,"bold"))
+Label1.grid(row = 0, column = 2)
+
+entry1 = Entry(root)
+entry1.grid(row = 1, column = 2)
+
+buttonEnter = Button(root, text = "Enter", command = lambda: PLACEHOLDER())
+buttonEnter.grid(row = 2, column = 2)
+
+
+
+#t1.start()
+#t2.start()
+
+root.mainloop()
